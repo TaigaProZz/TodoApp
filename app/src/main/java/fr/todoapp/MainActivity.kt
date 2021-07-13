e9b4132d598cd5306e49de6371c86cb4ca6da2fc
TaigaProZz <74510014+TaigaProZz@users.noreply.github.com>
2021-07-13 16:57:16 +0200

    pop up started

--- app/src/main/java/fr/todoapp/MainActivity.kt
+++ app/src/main/java/fr/todoapp/MainActivity.kt
@@ -1,11 +1,18 @@
 package fr.todoapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import fr.todoapp.tasklist.TaskListActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gotoTaskButton = findViewById<Button>(R.id.gotoTaskButton).setOnClickListener{
+            startActivity(Intent (applicationContext, TaskListActivity::class.java))
+        }
     }
 }
--- app/src/main/java/fr/todoapp/tasklist/TaskListActivity.kt
+++ app/src/main/java/fr/todoapp/tasklist/TaskListActivity.kt
@@ -1,12 +1,35 @@
 package fr.todoapp.tasklist
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import fr.todoapp.R
 
 class TaskListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_list)
+
+        // add button on the task activity list view
+        val addTaskButton = findViewById<Button>(R.id.add_task_button).setOnClickListener {
+            val myPopup = android.app.AlertDialog.Builder(this)
+
+
+            val input = EditText(this)
+            myPopup.setView(input)
+
+            myPopup.setTitle("Quelle tâche veux-tu ajouter?")
+
+            // add task button ( in the pop up )
+            myPopup.setPositiveButton("Ajouter"){ dialog, which ->
+                Toast.makeText(applicationContext, "Tâche bien ajoutée", Toast.LENGTH_SHORT).show()
+            }
+
+
+            myPopup.show()
+
+        }
     }
 }
