11cdda7abad7ebd8a24535179584b78dae645a59
TaigaProZz <74510014+TaigaProZz@users.noreply.github.com>
2021-07-15 12:48:12 +0200

    Popup done

--- app/src/main/java/fr/todoapp/CustomPopup.kt
+++ app/src/main/java/fr/todoapp/CustomPopup.kt
@@ -3,11 +3,10 @@ package fr.todoapp
 import android.app.Activity
 import android.app.Dialog
 
-class CustomPopup: Dialog() {
+class CustomPopup {
 
-    fun CustomPopup(activity: Activity) {
+    //fun CustomPopup(activity: Activity) {
 
-        super(activity, R.style.Theme_AppCompat_DayNight_Dialog)
+      //  super.(activity, R.style.Theme_AppCompat_DayNight_Dialog)
 
     }
-}
--- app/src/main/java/fr/todoapp/tasklist/TaskListActivity.kt
+++ app/src/main/java/fr/todoapp/tasklist/TaskListActivity.kt
@@ -1,5 +1,8 @@
 package fr.todoapp.tasklist
 
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.ViewGroup
@@ -16,22 +19,49 @@ class TaskListActivity : AppCompatActivity() {
 
         // add button on the task activity list view
         val addTaskButton = findViewById<Button>(R.id.add_task_button).setOnClickListener {
-            val myPopup = android.app.AlertDialog.Builder(this)
-            val input = EditText(this)
 
+            // call custom popup function when @addTaskButton is clicked
+            onCreateDialog(savedInstanceState)
 
-            myPopup.setView(input)
-
-            myPopup.setTitle("Quelle tâche veux-tu ajouter?")
-
-            // add task button ( in the pop up )
-            myPopup.setPositiveButton("Ajouter"){ dialog, which ->
-                Toast.makeText(applicationContext, "Tâche bien ajoutée", Toast.LENGTH_SHORT).show()
-            }
-
-
-            myPopup.show()
 
         }
     }
+
+    // function to create a custom popup
+    private fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        val activity = TaskListActivity::class.java
+
+        return activity.let {
+
+            val myPopupBuilder = AlertDialog.Builder(this@TaskListActivity)
+            val inflater = this.layoutInflater
+            val view = inflater.inflate(R.layout.activity_custom_popup, null, false)
+            val editText = view.findViewById<EditText>(R.id.editText_Popup)
+
+
+            // set the custom layout
+            myPopupBuilder.setView(view)
+
+            // set the title on the popup
+            myPopupBuilder.setTitle(R.string.title_popup)
+
+            // add the add task button
+            myPopupBuilder.setPositiveButton(
+                R.string.add_task
+            ) { dialog, which ->
+                // TODO add to the recycler view
+
+                // collect the text from the EditText
+                val task = editText.text.toString()
+                println(" the task is :$task")
+
+                Toast.makeText(this, "Task added", Toast.LENGTH_SHORT).show()
+            }
+
+            // show the popup
+            myPopupBuilder.show()
+        }
+    }
+    
 }
