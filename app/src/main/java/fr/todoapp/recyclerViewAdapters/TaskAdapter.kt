35f7b98e8019552f34eed0c3e77a60b4eb74b284
TaigaProZz <74510014+TaigaProZz@users.noreply.github.com>
2021-08-16 18:18:18 +0200

    creating menu for task

--- app/src/main/java/fr/todoapp/recyclerViewAdapters/TaskAdapter.kt
+++ app/src/main/java/fr/todoapp/recyclerViewAdapters/TaskAdapter.kt
@@ -3,6 +3,8 @@ package fr.todoapp.recyclerViewAdapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
+import androidx.compose.material.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.ktx.database
@@ -34,7 +36,6 @@ class TaskAdapter(val tasks: ArrayList<Task>) : RecyclerView.Adapter<TaskAdapter
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
-
         return ViewHolder(
             LayoutInflater.from(parent.context).inflate(
                 R.layout.task_adapter,
@@ -42,22 +43,33 @@ class TaskAdapter(val tasks: ArrayList<Task>) : RecyclerView.Adapter<TaskAdapter
                 false
             )
         )
-
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         val currentTask = tasks[position]
+        //viewHolder.itemView.setOnClickListener {
+        //    println("la position : $position")
 
-
-        viewHolder.itemView.setOnClickListener {
-            println("la position : $position")
-
-        }
-
-
+        //}
         viewHolder.itemView.apply {
             taskAdapterText.text = currentTask.taskName
             checkBoxAdapter.isChecked = currentTask.isChecked
+
+        }
+
+        viewHolder.itemView.setOnLongClickListener {
+
+            showPopup(it)
+            return@setOnLongClickListener true
+        }
+    }
+
+    @TODO
+    private fun showPopup(view: View){
+        this.let {
+            val popupMenu = PopupMenu(view.context, view)
+
+            popupMenu.menuInflater.inflate(R.layout.long_press_task, popupMenu.menu)
 
         }
 
